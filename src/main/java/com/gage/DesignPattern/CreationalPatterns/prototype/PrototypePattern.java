7d450c446f5299b4709a6b88a1adba0bb746cb84
fovegage <fovegage@gmail.com>
2023-06-02 10:13:35 +0800

    feat: prototype pattern

--- src/main/java/com/gage/DesignPattern/CreationalPatterns/prototype/PrototypePattern.java
+++ src/main/java/com/gage/DesignPattern/CreationalPatterns/prototype/PrototypePattern.java
@@ -2,6 +2,6 @@ package com.gage.DesignPattern.CreationalPatterns.prototype;
 
 public class PrototypePattern {
     public static void main(String[] args) {
-        // 原型模式 实现cloneable 复制二进制流
+        // 原型模式 实现cloneable复制二进制流，从而建设内存的开销
     }
 }
