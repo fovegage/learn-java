6c1d6e30382beb744e2b506495a7849dec4fb0e2
fovegage <fovegage@gmail.com>
2023-06-02 10:12:49 +0800

    feat: flyweight pattern

--- src/main/java/com/gage/DesignPattern/StructuralPatterns/flyweight/FlyweightPattern.java
+++ src/main/java/com/gage/DesignPattern/StructuralPatterns/flyweight/FlyweightPattern.java
@@ -2,6 +2,6 @@ package com.gage.DesignPattern.StructuralPatterns.flyweight;
 
 public class FlyweightPattern {
     public static void main(String[] args) {
-        // map保存对象
+        // map保存对象（将new过的对象进行保存到map中，如果new过那么直接从map中进行获取）
     }
 }
